d5dcb219a53019f681d2ca23614646f7481be419
Maria Nunez <mariaalician@gmail.com>
2018-05-12 00:09:13 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -48,3 +48,26 @@ describe('sayHiToGrandma(string)', function() {
     expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
   })
 })
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+  function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+console.log(string.toLowerCase())
+}
+
+//function sayHiToGrandma(string) {//
+  //return string.toUpperCase()//
+//}//
+
+var uppercase  = string
+
+ var showLove = "I love you, Grandma."
+ if (uppercase.toUpperCase() === uppercase){
+return("YES INDEED!") }
